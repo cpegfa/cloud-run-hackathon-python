596146f03ad99ff46924c5f43911f4bc98f7a6e4
cpegfa <137799304+cpegfa@users.noreply.github.com>
2023-06-26 16:38:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-moves = ['A', 'B', 'C', 'D']
+moves = ['F', 'R', 'L', 'T']
 
 @app.route("/", methods=['GET'])
 def index():
