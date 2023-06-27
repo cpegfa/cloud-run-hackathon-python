bc08cbd55a0a1e211ec73e46429fea8deb43cbcd
cpegfa <137799304+cpegfa@users.noreply.github.com>
2023-06-27 16:30:19 +0100

    Update game.py

--- game.py
+++ game.py
@@ -36,26 +36,31 @@ def get_game_state(infos, size=3):
 def highChanceHit():
     global prox
     global self
+    print("p:",prox)
     for i in range(len(prox)):
+        print("s",self)
+        if int(self['y']) == int(int(prox[i][1])) : 
+            print("Shoot")
+            return True
 
-        if int(self['x']) == prox[i][0] and int(self['y']) < prox[i][1] and self['direction'] == 'N': return True
-        if int(self['y']) == prox[i][1] and int(self['y']) > prox[i][0] and self['direction'] == 'W': return True
-        if int(self['y']) == prox[i][0] and int(self['y']) > prox[i][1] and self['direction'] == 'S': return True
-        if int(self['y']) == prox[i][1] and int(self['y']) < prox[i][0] and self['direction'] == 'E': return True
+        if int(self['x']) == int(prox[i][0]) and int(self['y']) < int(prox[i][1]) and self['direction'] == 'N': return True
+        if int(self['y']) == int(prox[i][1]) and int(self['x']) > int(prox[i][0]) and self['direction'] == 'W': return True
+        if int(self['x']) == int(prox[i][0]) and int(self['y']) > int(prox[i][1]) and self['direction'] == 'S': return True
+        if int(self['y']) == int(prox[i][1]) and int(self['x']) < int(prox[i][0]) and self['direction'] == 'E': return True
 
         match self['direction']:
             case 'N':
-                if int(self['y']) - 1 == prox[i][0] and int(self['y']) > prox[i][1] and prox[i][2] == 'E': return True
-                if int(self['y']) + 1 == prox[i][0] and int(self['y']) > prox[i][1] and prox[i][2] == 'W': return True
+                if int(self['x']) - 1 == int(prox[i][0]) and int(self['y']) > int(prox[i][1]) and prox[i][2] == 'E': return True
+                if int(self['x']) + 1 == int(prox[i][0]) and int(self['y']) > int(prox[i][1]) and prox[i][2] == 'W': return True
             case 'W':
-                if int(self['y']) - 1 == prox[i][1] and self['x'] > prox[i][0] and prox[i][2] == 'S': return True
-                if int(self['y']) + 1 == prox[i][1] and int(self['y']) > prox[i][0] and prox[i][2] == 'N': return True
+                if int(self['y']) - 1 == int(prox[i][1]) and int(self['x']) > int(prox[i][0]) and prox[i][2] == 'S': return True
+                if int(self['y']) + 1 == int(prox[i][1]) and int(self['x']) > int(prox[i][0]) and prox[i][2] == 'N': return True
             case 'S':
-                if int(self['y']) - 1 == prox[i][0] and int(self['y']) < prox[i][1] and prox[i][2] == 'E': return True
-                if int(self['y']) + 1 == prox[i][0] and int(self['y']) < prox[i][1] and prox[i][2] == 'W': return True
+                if int(self['x']) - 1 == int(prox[i][0]) and int(self['y']) < int(prox[i][1]) and prox[i][2] == 'E': return True
+                if int(self['x']) + 1 == int(prox[i][0]) and int(self['y']) < int(prox[i][1]) and prox[i][2] == 'W': return True
             case 'E':
-                if int(self['y']) - 1 == prox[i][1] and int(self['y']) < prox[i][0] and prox[i][2] == 'S': return True
-                if int(self['y']) + 1 == prox[i][1] and int(self['y']) < prox[i][0] and prox[i][2] == 'N': return True
+                if int(self['y']) - 1 == int(prox[i][1]) and int(self['x']) < int(prox[i][0]) and prox[i][2] == 'S': return True
+                if int(self['y']) + 1 == int(prox[i][1]) and int(self['x']) < int(prox[i][0]) and prox[i][2] == 'N': return True
     return False
     
 
@@ -64,19 +69,19 @@ def mustMove():
     global self
     global board_dims
     if self['wasHit'] == True:
-        if self['direction'] == 'N' and self['y'] > 0 : return True
-        if self['direction'] == 'W' and self['x'] > 0 : return True
-        if self['direction'] == 'S' and self['y'] < board_dims[1]-1 : return True
-        if self['direction'] == 'E' and self['x'] < board_dims[0]-1 : return True
+        if self['direction'] == 'N' and int(self['y']) > 0 : return True
+        if self['direction'] == 'W' and int(self['x']) > 0 : return True
+        if self['direction'] == 'S' and int(self['y']) < board_dims[1]-1 : return True
+        if self['direction'] == 'E' and int(self['x']) < board_dims[0]-1 : return True
     return False
      
 def cannotMove():
     global prox
     global self
     global board_dims
-    if self['direction'] == 'N' and self['y'] == 0 : return True
-    if self['direction'] == 'W' and self['x'] == 0 : return True
-    if self['direction'] == 'S' and self['y'] == board_dims[1]-1 : return True
-    if self['direction'] == 'E' and self['x'] == board_dims[0]-1 : return True
+    if self['direction'] == 'N' and int(self['y']) == 0 : return True
+    if self['direction'] == 'W' and int(self['x']) == 0 : return True
+    if self['direction'] == 'S' and int(self['y']) == board_dims[1]-1 : return True
+    if self['direction'] == 'E' and int(self['x']) == board_dims[0]-1 : return True
     
     
